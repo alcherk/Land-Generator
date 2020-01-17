5e96c9a0d8f3f859c5052f1546a26a1fc9d97dab
alcherk <aleksey.cherkassky@reviver.com>
2020-01-17 10:42:24 +0300

    initial project setup

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        if (DrawDefaultInspector() && mapGen.autoUpdate) {
+            mapGen.GenerateMap();
+        }
+
+        if (GUILayout.Button("Generate")) {
+            mapGen.GenerateMap();
+        }
+    }
+}
